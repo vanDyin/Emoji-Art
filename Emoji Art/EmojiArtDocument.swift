151c73a5c69ff9652637b498891b08a1acffe5c2
vanDyin <vycheslavoo2006@yandex.com>
2025-11-15 16:40:17 +0300

    Lecture 14

--- Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/EmojiArtDocument.swift
@@ -15,6 +15,11 @@ class EmojiArtDocument {
     private var emojiArt = EmojiArt() {
         didSet {
             autosave()
+            if emojiArt.background != oldValue.background {
+                Task {
+                    await fetchBackgroundImage()
+                }
+            }
         }
     }
     private var autosaveURL: URL =
@@ -30,9 +35,80 @@ class EmojiArtDocument {
     var emojis: [Emoji] {
         emojiArt.emojis
     }
+
+    var bbox: CGRect {
+        var bbox = CGRect.zero
+        for emoji in emojiArt.emojis {
+            bbox = bbox.union(emoji.bbox)
+        }
+        if let backgroundSize = background.uiImage?.size {
+            bbox = bbox.union(CGRect(center: .zero, size: backgroundSize))
+        }
+        return bbox
+    }
     
-    var background: URL? {
-        emojiArt.background
+    var background: Background = .none
+    
+    //MARK: - Background Image
+    
+    @MainActor
+    private func fetchBackgroundImage() async {
+        if let url = emojiArt.background {
+            background = .fetching(url)
+            do {
+                let image = try await fetchUIImage(from: url)
+                if url == emojiArt.background {
+                    background = .found(image)
+                }
+            } catch {
+                background = .failed("Could't set background: \(error.localizedDescription)")
+            }
+        } else {
+            background = .none
+        }
+    }
+    
+    private func fetchUIImage(from url: URL) async throws -> UIImage {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        if let uiImage = UIImage(data: data) {
+            return uiImage
+        } else {
+            throw FetchError.badImageData
+        }
+    }
+    
+    enum FetchError: Error {
+        case badImageData
+    }
+    
+    enum Background {
+        case none
+        case fetching(URL)
+        case found(UIImage)
+        case failed(String)
+        
+        var uiImage: UIImage? {
+            switch self {
+            case .found(let uiImage): return uiImage
+            default: return nil
+            }
+        }
+        
+        var urlBeingFetching: URL? {
+            switch self {
+            case .fetching(let url): return url
+            default: return nil
+            }
+        }
+        
+        var isFetching: Bool { urlBeingFetching != nil }
+        
+        var failureReason: String? {
+            switch self {
+            case .failed(let reason): return reason
+            default: return nil
+            }
+        }
     }
     
     private func autosave() {
@@ -91,11 +167,17 @@ extension EmojiArt.Emoji {
     var font: Font {
         Font.system(size: CGFloat(size))
     }
+    var bbox: CGRect {
+        CGRect (
+            center: position.in(nil),
+            size: CGSize(width: CGFloat(size), height: CGFloat(size))
+        )
+    }
 }
 
 extension EmojiArt.Emoji.Position {
-    func `in`(_ geometry: GeometryProxy) -> CGPoint {
-        let center = geometry.frame(in: .local).center
+    func `in`(_ geometry: GeometryProxy?) -> CGPoint {
+        let center = geometry?.frame(in: .local).center ?? .zero
         return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
     }
 }
